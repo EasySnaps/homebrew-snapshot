a8a1d6e0e25f0708a4d20d073f28796c7d6fa31b
DemonWeaver <199815038+DemonWeaver@users.noreply.github.com>
2026-07-27 10:17:46 -0400

    easysnaps 1.21.0: versioned DMG URL + real sha256

--- Casks/easysnaps.rb
+++ Casks/easysnaps.rb
@@ -1,8 +1,8 @@
 cask "easysnaps" do
   version "1.21.0"
-  sha256 :no_check
+  sha256 "d2a3f4a9ed0700212ba78e7d459166b3a7f962dec3d7474646443fe0115749cd"
 
-  url "https://easysnaps.org/downloads/EasySnaps.dmg"
+  url "https://easysnaps.org/downloads/EasySnaps-#{version}.dmg"
   name "EasySnaps"
   desc "Screen capture and screenshot editor for macOS"
   homepage "https://easysnaps.org"
